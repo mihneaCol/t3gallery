9a8c62e51b1ea19d06b24b411e3614fd095a7392
mihneaCol <mihnea.colita@gmail.com>
2024-05-18 14:22:13 +0300

    Database setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { db } from "~/server/db";
 
 const mockUrls = [
   "https://utfs.io/f/4b197969-b5e8-4a4f-bb5e-2880e65b7969-z7psj.jpg",
@@ -12,12 +13,19 @@ const mockImages = mockUrls.map((url, index) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  
+  const posts = await db.query.posts.findMany();
+  console.log(posts);
+
   return (
     <main className="">
       <div className="flex flex-wrap gap-4">
-        {[...mockImages, ...mockImages, ...mockImages].map((image) => (
-            <div key={image.id} className="w-48 p-4">
+        {posts.map((post) => (
+          <div  key={post.id}>{post.name}</div>
+        ))}
+        {[...mockImages, ...mockImages, ...mockImages].map((image, index) => (
+            <div key={image.id + "-" + index} className="w-48 p-4">
               <img src={image.url} />
             </div>
           ))
--- src/server/db/index.ts
+++ src/server/db/index.ts
@@ -4,6 +4,10 @@ import postgres from "postgres";
 import { env } from "~/env";
 import * as schema from "./schema";
 
+// Tutorial way of doing it, not working
+// import { sql } from "@vercel/postgres";
+// export const db = drizzle(sql, { schema });
+
 /**
  * Cache the database connection in development. This avoids creating a new connection on every HMR
  * update.
@@ -12,7 +16,7 @@ const globalForDb = globalThis as unknown as {
   conn: postgres.Sql | undefined;
 };
 
-const conn = globalForDb.conn ?? postgres(env.DATABASE_URL);
+const conn = globalForDb.conn ?? postgres(env.POSTGRES_URL);
 if (env.NODE_ENV !== "production") globalForDb.conn = conn;
 
 export const db = drizzle(conn, { schema });
